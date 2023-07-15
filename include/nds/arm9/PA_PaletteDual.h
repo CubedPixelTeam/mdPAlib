4b555437f1109606abc9c984a29f3550bbf03aa7
aerglass <thecatgamingpro562@gmail.com>
2023-07-15 14:37:01 -0400

    fix stuff here and there

--- include/nds/arm9/PA_PaletteDual.h
+++ include/nds/arm9/PA_PaletteDual.h
@@ -27,7 +27,7 @@
 /*! \def PA_DualLoadPal(palette, source)
     \brief
          \~english Load a 256 color palette in the Bg or Sprite palette of both screens
-         \~french Charger une palette de 256 couleurs pour les fonds ou les sprites pour les 2 écrans
+         \~french Charger une palette de 256 couleurs pour les fonds ou les sprites pour les 2 ï¿½crans
     \~\param palette
          \~english Set the Bg palette or Sprite palette : PAL_BG or PAL_SPRITE
          \~french Charger pour les Bg ou les Sprites : PAL_BG ou PAL_SPRITE
@@ -48,13 +48,13 @@
 /*! \def PA_DualLoadPal16(palette, n_palette, source)
     \brief
          \~english Load a 16 color palette in the Bg or Sprite palette of both screens
-         \~french Charger une palette de 16 couleurs pour les fonds ou les sprites pour les deux écrans
+         \~french Charger une palette de 16 couleurs pour les fonds ou les sprites pour les deux ï¿½crans
     \~\param palette
          \~english Set the Bg palette or Obj palette : PAL_BG or PAL_SPRITE
          \~french Charger pour les Bg ou les Sprites : PAL_BG ou PAL_SPRITE
     \~\param n_palette
          \~english Number of the 16 color palette to load (0-15)
-         \~french Numéro de la palette de 16 couleurs que l'on veut charger (0-15)
+         \~french Numï¿½ro de la palette de 16 couleurs que l'on veut charger (0-15)
 
     \~\param source
          \~english Palette name (ex : master_Palette)
@@ -70,7 +70,7 @@
 /*! \fn static inline void PA_DualSetPalNeg(u32 palette)
     \brief
          \~english Set all the palette's color to negative. To undo this, simply negative again...
-         \~french Négativer une palette donnée. Pour annuler, il suffit de négativer à nouveau. 
+         \~french Nï¿½gativer une palette donnï¿½e. Pour annuler, il suffit de nï¿½gativer ï¿½ nouveau. 
     \~\param palette
          \~english Set the Bg palette or Obj palette : PAL_BG, PAL_SPRITE
          \~french Charger pour les Bg ou les Sprites : PAL_BG, PAL_SPRITE
@@ -79,7 +79,7 @@ static inline void PA_DualSetPalNeg(u32 palette) {
 	u16* pal = (u16*)palette;
 	int i;
 	for (i = 0; i < 256; i++) {
-		pal[i+512] = pal[i] = ~pal[i]; // On fout le négatif...
+		pal[i+512] = pal[i] = ~pal[i]; // On fout le nï¿½gatif...
 	}
 }
 
@@ -89,19 +89,19 @@ static inline void PA_DualSetPalNeg(u32 palette) {
 /*! \fn static inline void PA_DualSetPal16Neg(u32 palette, u8 n_palette)
     \brief
          \~english Set 16 color palette to negative. To undo this, simply negative again...
-         \~french Négativer une palette de 16 couleurs donnée. Pour annuler, il suffit de négativer à nouveau. 
+         \~french Nï¿½gativer une palette de 16 couleurs donnï¿½e. Pour annuler, il suffit de nï¿½gativer ï¿½ nouveau. 
     \~\param palette
          \~english Set the Bg palette or Obj palette : PAL_BG, PAL_SPRITE
          \~french Charger pour les Bg ou les Sprites : PAL_BG, PAL_SPRITE
     \~\param n_palette
          \~english Number of the 16 color palette (0-15)
-         \~french Numéro de la palette de 16 couleurs (0-15)
+         \~french Numï¿½ro de la palette de 16 couleurs (0-15)
 */
 static inline void PA_DualSetPal16Neg(u32 palette, u8 n_palette) {
 	u16* pal = (u16*)palette;
 	int i;
 	for (i = (n_palette << 4); i < ((n_palette + 1) << 4); i++) {
-		pal[i+512] = pal[i] = ~pal[i]; // On fout le négatif...
+		pal[i+512] = pal[i] = ~pal[i]; // On fout le nï¿½gatif...
 	}
 }
 
@@ -111,10 +111,10 @@ static inline void PA_DualSetPal16Neg(u32 palette, u8 n_palette) {
          \~french Charger une palette de 256 couleurs dans les palettes des sprites
     \~\param palette_number
          \~english Palette number (0-15)
-         \~french Numéro de la palette (0-15)
+         \~french Numï¿½ro de la palette (0-15)
     \~\param palette
          \~english Palette to load ((void*)palette_name)
-         \~french Nom de la palette à charger ((void*)nom_palette)
+         \~french Nom de la palette ï¿½ charger ((void*)nom_palette)
 */
 static inline void PA_DualLoadSpritePal(u8 palette_number, void* palette){
 	PA_LoadSpriteExtPal(0, palette_number, palette);
@@ -127,10 +127,10 @@ static inline void PA_DualLoadSpritePal(u8 palette_number, void* palette){
          \~french Charger une palette de 256 couleurs pour un fond
     \~\param bg_number
          \~english Background number (0-3)
-         \~french Numéro du fond (0-3)
+         \~french Numï¿½ro du fond (0-3)
     \~\param palette
          \~english Palette to load ((void*)palette_name)
-         \~french Nom de la palette à charger ((void*)nom_palette)
+         \~french Nom de la palette ï¿½ charger ((void*)nom_palette)
 */
 static inline void PA_DualLoadBgPal(u8 bg_number, void* palette){
 	PA_LoadBgPal(0, bg_number, palette);
@@ -140,7 +140,7 @@ static inline void PA_DualLoadBgPal(u8 bg_number, void* palette){
 /*! \fn static inline void PA_DualSetBgColor(u16 color)
     \brief
          \~english Change the background color of both screens
-         \~french Changer la couleur de fond des 2 écrans
+         \~french Changer la couleur de fond des 2 ï¿½crans
     \~\param color
          \~english RGB value, like PA_RGB(31, 31, 31) for white
          \~french Valeur RGB, comme PA_RGB(31, 31, 31) pour blanc
